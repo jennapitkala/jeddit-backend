59f2599f45c7002b509e11563b3ed54899214a17
Jenna Pitkälä <jenna.pitkala@outlook.com>
2023-04-13 13:04:11 +0000

    subtle changes, nothing remarkable

--- subjeddits/models.py
+++ subjeddits/models.py
@@ -5,12 +5,9 @@ from django.urls import reverse
 class Subjeddit(models.Model):
     title = models.CharField(max_length=21)
     creation_date = models.DateTimeField('Created', default=datetime.datetime.now)
-    # this makes it look better in Subjeddit.objects.all() and in automatically created admin stuff
     slug = models.SlugField(null=False, unique=True)
     def __str__(self):
         return self.title
     def get_absolute_url(self):
         return reverse("index", kwargs={"slug": self.slug})
    
-#little change
-#this is main
