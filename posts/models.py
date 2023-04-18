1735286f947f4bec7d9f4fc2a1fa75b908877f12
Jenna Pitkälä <jenna.pitkala@outlook.com>
2023-04-18 11:32:30 +0000

    automated slugs, added slugify

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 from django.urls import reverse
+from django.template.defaultfilters import slugify # new
 
 from subjeddits.models import Subjeddit
 
@@ -11,9 +12,16 @@ class Post(models.Model):
     content = models.TextField(max_length=40000)
     creation_date = models.DateTimeField('Created', default=datetime.now)
     slug = models.SlugField(null=False, unique=True)
+    
     def __str__(self):
         return self.title
+    
     def get_absolute_url(self):
         return reverse("j:comments", kwargs={"slug": self.subjeddit.slug, "comments_slug": self.slug})
+    
+    def save(self, *args, **kwargs): #new function
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
     #author will come later
     #comments will come later
